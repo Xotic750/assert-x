ff1bef67634acde704e677861ca19fcf663340bc
Graham Fairweather <xotic750@gmail.com>
2019-07-17 00:20:13 +0200

    Terraformed
    Auto build

--- __tests__/assert-x.test.js
+++ __tests__/assert-x.test.js
@@ -1,28 +1,5 @@
 let assert;
 
-if (typeof module === 'object' && module.exports) {
-  require('es5-shim');
-  require('es5-shim/es5-sham');
-
-  if (typeof JSON === 'undefined') {
-    JSON = {};
-  }
-
-  require('json3').runInContext(null, JSON);
-  require('es6-shim');
-  const es7 = require('es7-shim');
-  Object.keys(es7).forEach(function(key) {
-    const obj = es7[key];
-
-    if (typeof obj.shim === 'function') {
-      obj.shim();
-    }
-  });
-  assert = require('../../index.js');
-} else {
-  assert = returnExports;
-}
-
 const a = assert;
 const hasSymbols = typeof Symbol === 'function' && typeof Symbol('') === 'symbol';
 
