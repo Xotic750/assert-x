da222dd76db1c18a1f6f5c21838dcda7a61d26ce
Graham Fairweather <xotic750@gmail.com>
2019-08-07 04:08:43 +0200

    Auto build

--- dist/assert-x.js
+++ dist/assert-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2015-2017",
-  "date": "2019-08-06T21:55:35.698Z",
+  "date": "2019-08-07T02:08:32.538Z",
   "describe": "",
   "description": "A Javascript assertion library.",
   "file": "assert-x.js",
